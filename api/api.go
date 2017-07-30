30e2b42626da7c715245e095c4fbb0c6bc46c6ca
Josh VanderLinden <codekoala@gmail.com>
2017-07-30 01:12:11 -0400

    Allow random quote by speaker

--- api/api.go
+++ api/api.go
@@ -1,11 +1,35 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/jackc/pgx"
 	"github.com/labstack/echo"
 	"go.uber.org/zap"
+)
+
+const (
+	QUERY = `
+		select
+			"s"."name" as "series",
+			"e"."title" as "title",
+			"e"."season",
+			"e"."episode",
+			"e"."airdate"::text,
+			"k"."name" as "speaker",
+			"l"."line"
+		from "line" "l"
+		join "episode" "e"
+		  on ("e"."id" = "l"."episode_id")
+		join "speaker" "k"
+		  on ("k"."id" = "l"."speaker_id" and "k"."series_id" = "e"."series_id")
+		join "series" "s"
+		  on ("s"."id" = "e"."series_id")
+		%s
+		order by random()
+		limit 1
+	`
 )
 
 var (
@@ -30,32 +54,30 @@ func Setup(l *zap.Logger, p *pgx.ConnPool) (app *echo.Echo) {
 	app = echo.New()
 
 	app.GET("/api/v1/random", RandomQuote)
+	app.GET("/api/v1/random/:speaker", RandomQuote)
 
 	return
 }
 
 func RandomQuote(c echo.Context) (err error) {
-	var quote Quote
+	var (
+		quote Quote
+		query string
+		row   *pgx.Row
 
-	err = pool.QueryRow(`
-		select
-			s.name as series,
-			e.title as title,
-			e.season,
-			e.episode,
-			e.airdate::text,
-			k.name as speaker,
-			l.line
-		from line l
-		join episode e
-		  on (e.id = l.episode_id)
-		join speaker k
-		  on (k.id = l.speaker_id and k.series_id = e.series_id)
-		join series s
-		  on (s.id = e.series_id)
-		order by random()
-		limit 1
-	`).Scan(
+		speaker = c.Param("speaker")
+	)
+
+	if speaker != "" {
+		query = fmt.Sprintf(QUERY, `where "k"."name" = $1`)
+		row = pool.QueryRow(query, speaker)
+	} else {
+		query = fmt.Sprintf(QUERY, "")
+		row = pool.QueryRow(query)
+	}
+	log.Debug("executing query", zap.String("query", query))
+
+	err = row.Scan(
 		&quote.Series,
 		&quote.Title,
 		&quote.Season,
